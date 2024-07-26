8bbd750f26cfe22fe0074d1236d085604ac67f77
HhRyo <88109093+HhRyo@users.noreply.github.com>
2024-07-26 02:32:07 -0400

    Update NodeType.java

--- NodeType.java
+++ NodeType.java
@@ -2,4 +2,24 @@ public class NodeType<T extends Comparable<T>> {
 public T info;
 public NodeType<T> left;
 public NodeType<T> right;
+
+public NodeType(T info){
+  Info = info;
+}
+
+public bool isLeafNode(){
+  if(left != null && right != null){
+  return true;
+  }
+  return false;
+}
+
+public bool isSingleParent(){
+  if(left != null && right == null || left == null && right != null ){
+  return true;
+  }
+  return false;
+}
+
+
 } // NodeType
