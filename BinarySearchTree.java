5768ba3104bf5a7e2362143616aee18ec7ebf28d
HhRyo <88109093+HhRyo@users.noreply.github.com>
2024-07-26 00:56:12 -0400

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,1 +1,37 @@
+public class BinarySearchTree<T extends Comparable<T>> {
+    private NodeType<T> root;
 
+    public BinarySearchTree() {
+        this.root = null;
+    } // binarySearchTree
+
+    public void insert(T key) {
+
+    } // insert
+
+    public void delete(T key) {
+
+    } // delete
+
+    public boolean search(T item) {
+        throw new UnsupportedOperationException("not yet implemented");
+    } // search
+
+    public void inOrder() {
+
+    } // inOrder
+
+    public void getSingleParent() {
+
+    } // getSingleParent
+
+    public int getNumLeafNodes() {
+        throw new UnsupportedOperationException("not yet implemented");
+    } // getNumLeafNodes
+
+    public T getCousins(NodeType<T> node) {
+        //putting T as return type bc I assume we're returning the info of the cousin
+        throw new UnsupportedOperationException("not yet implemented");
+    } // getCousins
+
+} // BinarySearchTree
