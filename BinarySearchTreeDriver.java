2f12b4a8849886de8befa0d65bb8a921a4f5bdaa
HhRyo <88109093+HhRyo@users.noreply.github.com>
2024-07-31 02:19:36 -0400

    Update BinarySearchTreeDriver.java

--- BinarySearchTreeDriver.java
+++ BinarySearchTreeDriver.java
@@ -64,7 +64,7 @@ public class BinarySearchTreeDriver {
             // Insert
             if(userInput.equals("i")){
                 System.out.println("Enter a value to insert: ");
-                inputVal = listScan.next();
+                isTrue = list.search(Integer.parseInt(inputVal));
                 if(list.search(inputVal) == true){
                     System.out.println("The item already exists in the tree.");
                 }
